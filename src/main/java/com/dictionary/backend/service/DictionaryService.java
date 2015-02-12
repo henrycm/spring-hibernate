d5c599261fa5a4c54cd2cc4f30391a385af25449
henrycm <henrycm@gmail.com>
2015-02-12 09:56:50 -0500

    Synchronize save method to avoid saving exception

--- src/main/java/com/dictionary/backend/service/DictionaryService.java
+++ src/main/java/com/dictionary/backend/service/DictionaryService.java
@@ -36,7 +36,7 @@ public class DictionaryService {
 			return new ArrayList<Meaning>();
 	}
 
-	public void save(Entry e) {
+	public synchronized void save(Entry e) {
 		List<Meaning> l = e.getMeanings();
 		Entry en = er.findOne(e.getWord());
 		if (en == null) {
